a763caa29349dfb5b18afac7f8e4f4e02b04cd23
Johanna Florange <florangejohanna@gmail.com>
2022-05-30 17:41:31 +0200

    routes, controllers, view page added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :books do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :chatrooms, only: [:index, :show, :new, :create] do
+    resources :messages, only: [:new, :creatye]
+  end
 end
