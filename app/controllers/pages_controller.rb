8266dcb9e2c9a17f95077a865415af2736051c4d
Frederik Feldmeier <f.feldmeier98@googlemail.com>
2022-05-30 17:53:04 +0200

    added font styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :component ]
 
   def home
+  end
+
+  def component
   end
 end
