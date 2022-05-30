060b991018cf647f5b34975e11ee68716ebe5388
Frederik Feldmeier <f.feldmeier98@googlemail.com>
2022-05-30 18:07:48 +0200

    solved authenticate issue

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :component ]
+  # before_action :authenticate_user!, only: [ :home, :component ]
 
   def home
   end
