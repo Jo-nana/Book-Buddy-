899ea464977a9519a6a9b00ad030364d5b38bfd0
Johanna Florange <florangejohanna@gmail.com>
2022-05-31 14:37:20 +0200

    search form & tags "form" added in the index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,20 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+
+    # search bar
+
+    if params[:query].present?
+      @books = Book.search_by_title_and_author(params[:query])
+    elsif params[:tag].present?
+      @books = Book.where("'#{params[:tag]}' = ANY (tags)")
+    else
+      @books = Book.all
+    end
+    @search_by_tags = Book.where(tags: params[:query])
+    # link to with where tags = paramsquery
+
+    @tags = ["Horror", "S-F", "Distopia", "Classic", "Adventure", "Fantasy"]
   end
 
   def show
