dc90d05d00f4fd34b6a265c49709aabaadca0123
Johanna Florange <florangejohanna@gmail.com>
2022-06-02 12:03:43 +0200

    cloudinary set up

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,6 +1,6 @@
 class Chatroom < ApplicationRecord
   belongs_to :user
   belongs_to :book
-  has_many :messages
+  has_many :messages, dependent: :destroy
   validates :name, presence: true, uniqueness: true
 end
