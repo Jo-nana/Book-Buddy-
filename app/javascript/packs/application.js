6e298aa322057cdc7ae36dd7e42856627d52255b
Frederik Feldmeier <f.feldmeier98@googlemail.com>
2022-06-02 17:44:58 +0200

    solved js issue and h2 issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,12 +15,12 @@ ActiveStorage.start()
 import "controllers"
 import "bootstrap"
 
-scroll = document.querySelector(".down-arrow")
-scroll.addEventListener("click",(event) => {
-  event.preventDefault()
- console.log("xwxwx")
- window.scrollTo(0, document.body.scrollHeight);
-})
+// scroll = document.querySelector(".down-arrow")
+// scroll.addEventListener("click",(event) => {
+//   event.preventDefault()
+//  console.log("xwxwx")
+//  window.scrollTo(0, document.body.scrollHeight);
+// })
 
 import { init_showButton } from '../plugins/init_showButton';
 document.addEventListener('turbolinks:load', () => {
