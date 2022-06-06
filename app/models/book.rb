61b5fd589e9ae9497be8b8db114071d3259c88ae
Johanna Florange <florangejohanna@gmail.com>
2022-06-06 17:33:26 +0200

    fixing bug with ajax

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :chatroom, dependent: :destroy
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
 
   validates :title, :author, :tags, :picture, presence: true
