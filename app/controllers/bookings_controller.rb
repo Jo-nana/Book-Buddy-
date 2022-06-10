1afb67619bcb28c6d76e9d7094e91e635db8de05
Johanna Florange <florangejohanna@gmail.com>
2022-06-10 14:34:21 +0200

    opening chatroom after accept or swapping, code has been changed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -21,8 +21,11 @@ class BookingsController < ApplicationController
     @booking.accepted = true
     @booking.save
     @booking.book.change_availability_to_false
-    chat = Chatroom.create!(user: @booking.user, book: @booking.book, name: @booking.book.user.first_name + " & " + current_user.first_name, bookclub: false)
+    chat = Chatroom.create!(user: @booking.user, book: @booking.book, name: " #  #{rand(1..100).to_s}", bookclub: false)
+    Message.create!(content: "Thanks for accepting my request", chatroom: chat, user: @booking.user)
     redirect_to chatroom_path(chat), notice: "Request accepted!"
+    # chat = Chatroom.create!(user: @booking.user, book: @booking.book, name: @booking.book.user.first_name + " & " + current_user.first_name, bookclub: false)
+    # redirect_to chatroom_path(chat), notice: "Request accepted!"
   end
 
   def swap
@@ -38,7 +41,10 @@ class BookingsController < ApplicationController
     @booking_one.user = current_user
     @booking_one.save!
     @booking_one.book.change_availability_to_false
-    redirect_to dashboard_path, notice: "It's a swap!"
+    # redirect_to dashboard_path, notice: "It's a swap!"
+    chat = Chatroom.create!(user: @booking.user, book: @booking.book, name: " #  #{rand(1..100).to_s}", bookclub: false)
+    Message.create!(content: "Thanks for accepting the swap!", chatroom: chat, user: @booking.user)
+    redirect_to chatroom_path(chat), notice: "It's a swap!"
   end
 
 
