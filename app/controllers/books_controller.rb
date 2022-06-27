3ed703b135b98c66f93dbd37b9d7d858b3cf1963
Johanna Florange <florangejohanna@gmail.com>
2022-06-27 13:33:41 +0200

    books controller code cleaned

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,13 @@
 class BooksController < ApplicationController
   def index
-    if params[:dissapear].present?
-      @dissapear = params[:dissapear]
-    end
+    @dissapear = params[:dissapear] if params[:dissapear].present?
     @books = Book.all
     @tags = ["Horror", "S-F", "Distopia", "Classic", "Adventure", "Fantasy"]
-    # search bar
+    # Search bar
     if params[:query].present?
       @books = Book.search_by_title_and_author(params[:query])
     elsif params[:tag].present?
-      # @books = Book.where("'#{params[:tag]}' = ANY (tags)")
-      @books = Book.where("tags @> ARRAY[?]::varchar[]", ["#{params[:tag]}", "#{params[:tag]}"])
+      @books = Book.where("tags @> ARRAY[?]::varchar[]", [params[:tag].to_s, params[:tag].to_s])
     else
       @books = Book.all
     end
@@ -24,13 +21,11 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @chatrooms = Chatroom.all
-    @books_tag = Book.where("tags @> ARRAY[?]::varchar[]", ["#{@book.tags[0]}", "#{@book.tags[1]}"])
+    @books_tag = Book.where("tags @> ARRAY[?]::varchar[]", [@book.tags[0].to_s, @book.tags[1].to_s])
 
-    # code to see all the users on map (could be usefull later)
-
+    # Code to see all the users on map (could be usefull later)
     # user_ids = Book.all.select('user_id').pluck(:user_id).uniq
     # @users = User.where(id: user_ids)
-
     # geocoder works with an array so let's put the user_id in an array
 
     @users = User.where(id: [@book.user.id])
@@ -38,7 +33,6 @@ class BooksController < ApplicationController
       {
         lat: user.latitude,
         lng: user.longitude,
-        # info_window: render_to_string(partial: "info_window", locals: { flat: flat }),
         image_url: helpers.asset_url("book.png")
       }
     end
@@ -57,7 +51,6 @@ class BooksController < ApplicationController
     else
       render :new
     end
-
   end
 
   def edit
@@ -85,33 +78,4 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :author, :description, :year, :photo, :tags)
   end
-  # def calculate_distance
-  #   @delta = Geocoder::Calculations.distance_between([current_user.latitude, current_user.longitude], [book.user.latitude, book.user.longitude])
-  # end
 end
-
-# distance between two points (haversine formula)
-# module Geocoder
-#   module Calculations
-#     extend self
-#       def distance_between(current_user.address, book., options = {})
-#         # convert to coordinate arrays
-#         point1 = extract_coordinates(point1)
-#         point2 = extract_coordinates(point2)
-
-#         # convert degrees to radians
-#         point1 = to_radians(point1)
-#         point2 = to_radians(point2)
-
-#         # compute deltas
-#         dlat = point2[0] - point1[0]
-#         dlon = point2[1] - point1[1]
-
-#         a = (Math.sin(dlat / 2))**2 + Math.cos(point1[0]) *
-#             (Math.sin(dlon / 2))**2 * Math.cos(point2[0])
-#         c = 2 * Math.atan2( Math.sqrt(a), Math.sqrt(1-a))
-#         c * earth_radius(options[:units])
-#       end
-#     end
-#   end
-# end
